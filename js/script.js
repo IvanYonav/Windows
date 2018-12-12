1cafd8f039ca359862df8dbd7497e59b5f0ea0da
Ivan <43743282+IvanYonav@users.noreply.github.com>
2018-12-12 23:44:23 +0500

    Добавленны модальные окна обратной связи

--- js/script.js
+++ js/script.js
@@ -1,29 +1,43 @@
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
-//popup engineer
-    
-let button = document.querySelector('.header_btn'),
-    popupEngineer = document.querySelector('.popup_engineer'),
-    overlay = document.querySelector('.overlay'),
-    popupEngClose = document.getElementsByClassName('popup_close')[1];
+    //popup engineer
 
+    let button = document.querySelector('.header_btn'),
+        popupEngineer = document.querySelector('.popup_engineer'),
+        overlay = document.querySelector('.overlay'),
+        popupClose = document.querySelectorAll('.popup_close'),
+        phoneLink = document.querySelectorAll('.phone_link'),
+        popup = document.querySelector('.popup');
 
-button.addEventListener('click', function () {
-    overlay.style.display = 'block';
-    popupEngineer.style.display = 'block';
-    document.body.style.overflow = "hidden";
+    function showModal(popupName) {
+        overlay.style.display = 'block';
+        popupName.style.display = 'block';
+        document.body.style.overflow = "hidden";
+    }
 
-});
+    function hideModal(popupName) {
+        popupName.style.display = 'none';
+        overlay.style.display = 'none';
+        document.body.style.overflow = "";
+    }
 
-popupEngClose.addEventListener('click', function () {
-    popupEngineer.style.display = 'none';
-    overlay.style.display = 'none';
-    document.body.style.overflow = "";
+    button.addEventListener('click', function() {
+        showModal(popupEngineer);
+    });
+
+    phoneLink.forEach(function(item) {
+        item.addEventListener('click', function () {
+            showModal(popup);
+        });
+    });
+
+    popupClose[1].addEventListener('click', function () {
+        hideModal(popupEngineer);
+    });
+
+    popupClose[0].addEventListener('click', function () {
+        hideModal(popup);
     });
 
 });
-
-
-
-
