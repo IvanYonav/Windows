d8eb9c5fe6de91b3a15c614aa7d234b43ec4b49a
Ivan <43743282+IvanYonav@users.noreply.github.com>
2018-12-15 02:06:06 +0500

    Доработанны табы

--- js/script.js
+++ js/script.js
@@ -115,8 +115,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //табы с отделкой
 
-    let decorTab = document.querySelectorAll('.decoration_item'),
-        decorTabLink = document.querySelectorAll('.decoration_links'),
+    let decorTabLink = document.querySelectorAll('.decoration_links'),
+        decorTab = document.querySelectorAll('.decoration_inItem'),
         decorTabParent = document.querySelector('.decoration_slider'),
         decorTabContent = document.querySelectorAll('.decoration_types');
 
@@ -124,34 +124,30 @@ window.addEventListener('DOMContentLoaded', () => {
         for (let i = a; i < decorTabContent.length; i++) {
             decorTabContent[i].classList.remove('show');
             decorTabContent[i].classList.add('hide');
+            decorTab[i].classList.remove('after_click');
         }
     };
+
     decorHideTabContent(1);
 
     let decorShowTabContent = (b) => {
         if (decorTabContent[b].classList.contains('hide')) {
             decorTabContent[b].classList.remove('hide');
             decorTabContent[b].classList.add('show');
+            decorTab[b].classList.add('after_click');
         }
     };
 
-    function decorSortTab(tabName, target) {
-        for (let i = 0; i < tabName.length; i++) {
-            if (target == tabName[i]) {
-                decorHideTabContent(0);
-                decorShowTabContent(i);
-            }
-        }
-    }
-
     decorTabParent.addEventListener('click', (event) => {
         let target = event.target;
-        if (target && target.classList.contains('decoration_item')) {
-            decorSortTab(decorTab, target);
-        } else if (target && target.classList.contains('decoration_links')) {
-            decorSortTab(decorTabLink, target);
+        if (target && target.classList.contains('decoration_links')) {
+            for (let i = 0; i < decorTabLink.length; i++) {
+                if (target == decorTabLink[i]) {
+                    decorHideTabContent(0);
+                    decorShowTabContent(i);
+                }
+            }
         }
-
     });
 
     //timer
